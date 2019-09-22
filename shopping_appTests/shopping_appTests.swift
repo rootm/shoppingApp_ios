b95f769ac44db0ff846c83593d148483d8246e6a
rootm <muvindu.t@gmail.com>
2019-09-22 11:53:54 +0530

    Initial Commit

---
+++ shopping_appTests/shopping_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  shopping_appTests.swift
+//  shopping_appTests
+//
+//  Created by Muvindu on 9/22/19.
+//  Copyright © 2019 Muvindu. All rights reserved.
+//
+
+import XCTest
+@testable import shopping_app
+
+class shopping_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
